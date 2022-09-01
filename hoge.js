a8c188e85683de87cd11df5284fcf53652e2b3d7
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-09-01 19:12:00 +0900

    Promiseを追加

--- hoge.js
+++ hoge.js
@@ -1,5 +1,15 @@
-let hogeValue = 1;
-hogeValue = "文字を代入できる型定義がないから!";
-console.log(hogeValue);
-let message = ["怖いですね〜😅", "怖いですね〜😅", "怖いですね〜😅"];
-message.forEach((e) => console.log(e));
+Promise.resolve()
+.then(() => {
+  new Promise((resolve) => {
+    setTimeout(() => {
+      let hogeValue = 1;
+      hogeValue = "文字を代入できる型定義がないから!";
+      console.log(hogeValue);
+      let message = ["怖いですね〜😅", "怖いですね〜😅", "怖いですね〜😅"];
+      message.forEach((e) => console.log(e));
+      resolve();
+    }, 1000);
+  })
+});
+const messageLog = "1秒後にコールバックを実行する!";
+console.log(`これはログです: ${messageLog}`);
